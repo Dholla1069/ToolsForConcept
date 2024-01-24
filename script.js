1fc41347c644b2fc825dd51f6254b533ab5a6fb6
Dholla1069 <117790608+Dholla1069@users.noreply.github.com>
2024-01-23 22:53:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const categories = {
+    category1: ['space', 'wild west', 'robot', 'martial arts', 'punk', 'prehistoric', 'futuristic', 'jungle', 'animal', 'undead', 'circus', 'plant'],
+    category2: ['bow', 'bottle', 'rocket-launcher', 'boomerang', 'grenade', 'shot gun', 'pistol', 'rifle', 'hammer', 'bat'],
+    category3: ['arrows', 'lasers', 'grenades', 'fire', 'poison', 'cacti', 'sticky-blob', 'snow', 'blades', 'electricity', 'smoke']
+};
+
+function getRandomElement(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+function generatePhrase() {
+    const outputElement = document.getElementById('output');
+    
+    const randomWord = getRandomElement(categories.category1);
+    const randomPhrase = getRandomElement(categories.category2);
+    const randomSentence = getRandomElement(categories.category3);
+
+    const generatedPhrase = `This new brawler has a ${randomWord} theme, they prefer a ${randomPhrase} weapon and their choice of projectile is ${randomSentence}.`;
+
+    outputElement.textContent = generatedPhrase;
+}
